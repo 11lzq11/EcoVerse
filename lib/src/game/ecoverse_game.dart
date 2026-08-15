df9be7fd440e8d9deb6cb39def9a142ac052c4d5
11lzq11 <lzq09052012@gmail.com>
2026-08-16 04:54:19 +0800

    Remove HasKeyboardHandlerComponents and unused imports

--- lib/src/game/ecoverse_game.dart
+++ lib/src/game/ecoverse_game.dart
@@ -1,9 +1,6 @@
 import 'package:flame/game.dart';
-import 'package:flutter/services.dart';
 
-class EcoVerseGame extends FlameGame with HasKeyboardHandlerComponents {
-  final Map<LogicalKeyboardKey, bool> keysDown = {};
-
+class EcoVerseGame extends FlameGame {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
@@ -12,16 +9,5 @@ class EcoVerseGame extends FlameGame with HasKeyboardHandlerComponents {
   @override
   void update(double dt) {
     super.update(dt);
-  }
-
-  @override
-  KeyEventResult onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keys) {
-    final key = event.logicalKey;
-    if (event is RawKeyDownEvent) {
-      keysDown[key] = true;
-    } else if (event is RawKeyUpEvent) {
-      keysDown[key] = false;
-    }
-    return super.onKeyEvent(event, keys);
   }
 }
